9d858b1706668b09b955b2b1ccd903c286c1fab0
visual <visual@LAB24-03>
2021-12-07 15:25:39 +0300

    Добавьте файлы проекта.

---
+++ App7.2/App7.2/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App7._2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), TimeTick);
+        }
+        bool run = false;
+        string message = "";
+        DateTime setDate;
+
+        private void bth_s_Clicked(object sender, EventArgs e)
+        {
+            setDate = dataPic.Date + time.Time;
+            message = ent_M.Text;
+            dataPic.IsEnabled = false;
+            time.IsEnabled = false;
+            ent_M.IsEnabled = false;
+            run = true;
+        }
+
+        private void bth_sp_Clicked(object sender, EventArgs e)
+        {
+            setDate = DateTime.MinValue;
+            dataPic.IsEnabled = true;
+            time.IsEnabled = true;
+            ent_M.IsEnabled = true;
+            run = false;
+        }
+
+
+        bool TimeTick()
+        {
+            if (run)
+            {
+                if (setDate <= DateTime.Now)
+                {
+                    DisplayAlert("Напиминание", message, "Ok");
+                    run = false;
+                    dataPic.IsEnabled = true;
+                    time.IsEnabled = true;
+                    ent_M.IsEnabled = true;
+                }
+            }
+            return true;
+        }
+    }
+}
+
